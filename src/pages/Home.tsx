8065f7e61c2ee386d1ad49fc1fc32e9a0ae70a04
Daniel Geng <dangengdg@gmail.com>
2022-07-16 09:17:15 -0700

    Made pretty shit landing page (#2)

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,4 +1,5 @@
 import { Box, Typography, TextField, Button } from '@mui/material'
+import Grid from '@mui/material/Grid';
 import { Link } from 'react-router-dom'
 import routes from './routes'
 import Wrapper from '../components/Wrapper'
@@ -10,79 +11,35 @@ import Database from '../types/Database'
 import { ConfessForm } from '../components/ConfessForm'
 
 const Home = () => {
-  const {
-    register,
-    handleSubmit,
-    reset,
-    formState: { errors }
-  } = useForm<{ name: string }>()
-
-  const [todos, setTodos] = useState<Database.Todo[]>([])
 
   return (
     <>
-      <AppBar
-        title='fullstrapp'
-        actions={
-          <Button
-            color='primary'
-            size='small'
-            component={Link}
-            to={routes.signin}
-            variant='contained'
-          >
-            Sign In
-          </Button>
-        }
-      />
-      <Wrapper>
-        <ConfessForm />
-        <Typography paragraph variant='h5'>
-          Welcome to your new app!
-        </Typography>
-
-        <Typography paragraph variant='h5'>
-          Don't forget to configure your firebase settings in{' '}
-          <code>/src/initFirebase.ts</code>
-        </Typography>
-
-        <Box mt={6}>
-          <Typography paragraph>TODO List</Typography>
-          <ul>
-            {todos.map(todo => (
-              <li>{todo.description}</li>
-            ))}
-          </ul>
-        </Box>
-        <form
-          onSubmit={handleSubmit(vals => {
-            setTodos([
-              ...todos,
-              {
-                id: vals.name,
-                description: vals.name,
-                order: todos.length,
-                completed: false
-              }
-            ])
-            reset()
-          })}
+      <Box style={{backgroundColor: "rgb(152,190,250)", height: '100vh', width: '100vw'}}>
+        <Grid
+          container
+          direction="column"
+          justifyContent="center"
+          alignItems="center"
+          style={{height: '100vh'}}
         >
-          <TextField
-            label='Enter your name'
-            variant='outlined'
-            fullWidth
-            {...register('name', {
-              required: formErrorMessages.required
-            })}
-            error={!!errors.name}
-            helperText={errors.name?.message || ' '}
-          />
-          <Button type='submit' color='primary'>
-            Submit
-          </Button>
-        </form>
-      </Wrapper>
+          <Typography paragraph variant='h1' align='center'>
+            College Crush <br></br>Confessions
+          </Typography>
+
+          <Box style={{paddingTop: '10vh', marginLeft: 'auto', marginRight: 'auto', width: '80%'}}>
+            <Typography paragraph variant='body2' align='center'>
+              Have a crush? Too scared to confess? Put down their “.edu” email address and get notified <i>only if they put yours down too</i>! <b>Completely anonymous, completely confidential</b>.
+            </Typography>
+          </Box>
+        </Grid>
+
+        <Typography paragraph align='center' style={{position: 'absolute', bottom: '0', fontSize: '18pt', left: '0', right: '0'}}>
+        ⬇️ See the Leaderboards ⬇️
+        </Typography>
+      </Box>
+      <Box style={{backgroundColor: "rgb(250,211,152)", height: '100vh', width: '100vw', padding: '30px'}}>
+        <ConfessForm />
+      </Box>
     </>
   )
 }
