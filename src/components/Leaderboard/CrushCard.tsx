dfdeda0f1b21adbfd24e75da6a4403d53c43b3e2
MaxYuan85 <maxyuan85@gmail.com>
2022-07-16 22:24:25 -0700

    add props to component (#9)
    Co-authored-by: Max Yuan <maxyuan@gmail.com>

--- src/components/Leaderboard/CrushCard.tsx
+++ src/components/Leaderboard/CrushCard.tsx
@@ -1,19 +1,50 @@
 import * as React from 'react';
-import Box from '@mui/material/Box';
 import Card from '@mui/material/Card';
-import CardActions from '@mui/material/CardActions';
 import CardContent from '@mui/material/CardContent';
-import Button from '@mui/material/Button';
 import Typography from '@mui/material/Typography';
 import Grid from '@mui/material/Grid';
 
-// Comments shit
-import IconButton from '@mui/material/IconButton';
+import { IconButton } from '@mui/material';
 import FavoriteBorderIcon from '@mui/icons-material/FavoriteBorder';
-import { CrushComment } from './CrushComment'
 
-export const CrushCard = () => {
+export interface CrushCommentInterface {
+  id: string
+  comment: string
+  likes: number
+}
 
+export interface CrushCardInterface {
+  email: string
+  crushCount: number,
+  instagramLink?: string
+  linkedinLink?: string
+  confessions: CrushCommentInterface[]
+}
+
+export const CrushComment = (props: CrushCommentInterface) => {
+  const { id, comment, likes } = props;
+
+  return (
+    <Grid container wrap="nowrap" alignItems="center">
+      <Grid item sx={{ mb: 1, mt: 1 }}>
+        <Typography component="div">
+          {comment}
+        </Typography>
+      </Grid>
+      <Grid item>
+        <IconButton color="primary" component="label">
+          <FavoriteBorderIcon />
+        </IconButton>
+      </Grid>
+    </Grid>
+  );
+
+}
+
+
+
+export const CrushCard = (props: CrushCardInterface) => {
+  const { email, crushCount, instagramLink, linkedinLink, confessions } = props;
   const card = (
     <React.Fragment>
       <CardContent>
@@ -21,28 +52,36 @@ export const CrushCard = () => {
           Email
         </Typography>
         <Typography variant="h5" component="div">
-          mary.jane@berkeley.edu
+          {email}
         </Typography>
         <Typography sx={{ mt: 1.5 }} variant="subtitle1" color="text.secondary">
           Crush Count
         </Typography>
         <Typography variant="h5" component="div">
-          7
+          {crushCount}
         </Typography>
         <Typography sx={{ mt: 1.5 }} variant="subtitle1" color="text.secondary">
           Social Media
         </Typography>
         <Typography variant="h5" component="div">
-          instagram.com/maryjane
+          {instagramLink}
         </Typography>
         <Typography sx={{ mt: 1.5 }} variant="subtitle1" color="text.secondary">
           Love Notes
         </Typography>
 
         <Grid container wrap="nowrap" direction="column">
-          <CrushComment />
-          <CrushComment />
-          <CrushComment />
+          {
+            confessions.map(confession => {
+              return (
+                <CrushComment
+                  id={confession.id}
+                  comment={confession.comment}
+                  likes={confession.likes}
+                />
+              )
+            })
+          }
         </Grid>
 
       </CardContent>
@@ -51,9 +90,9 @@ export const CrushCard = () => {
 
   return (
     <Grid item xs={12} md={6} lg={4}>
-      <Card variant="outlined" sx={{ borderRadius: '20px', padding: '10px', boxShadow: 3}}>{card}</Card>
+      <Card variant="outlined" sx={{ borderRadius: '20px', padding: '10px', boxShadow: 3 }}>{card}</Card>
     </Grid>
   );
-  
+
 }
 
