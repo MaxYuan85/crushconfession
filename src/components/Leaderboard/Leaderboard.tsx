8b1541e49094a3b685613808152699cee0426977
Max Yuan <maxyuan@gmail.com>
2022-07-16 22:23:09 -0700

    add props to component

--- src/components/Leaderboard/Leaderboard.tsx
+++ src/components/Leaderboard/Leaderboard.tsx
@@ -2,25 +2,102 @@ import Grid from '@mui/material/Grid';
 import { CrushCard } from './CrushCard';
 
 export const Leaderboard = () => {
-  
+  const testData = [
+    {
+      email: 'maryjane@email.com',
+      crushCount: 5,
+      instagramLink: 'insta.com',
+      confessions: [
+        {
+          id: 'asdf',
+          comment: 'you are the best love lve love',
+          likes: 2,
+        },
+        {
+          id: 'asdf',
+          comment: 'you are the best love lve love',
+          likes: 2,
+        },
+        {
+          id: 'asdf',
+          comment: 'you are the best love lve love',
+          likes: 2,
+        }
+      ]
+    },
+    {
+      email: 'maryjane@email.com',
+      crushCount: 5,
+      instagramLink: 'insta.com',
+      confessions: [
+        {
+          id: 'asdf',
+          comment: 'you are the best love lve love',
+          likes: 2,
+        },
+        {
+          id: 'asdf',
+          comment: 'you are the best love lve love',
+          likes: 2,
+        },
+        {
+          id: 'asdf',
+          comment: 'you are the best love lve love',
+          likes: 2,
+        }
+      ]
+    },
+    {
+      email: 'maryjane@email.com',
+      crushCount: 5,
+      instagramLink: 'insta.com',
+      confessions: [
+        {
+          id: 'asdf',
+          comment: 'you are the best love lve love',
+          likes: 2,
+        },
+        {
+          id: 'asdf',
+          comment: 'you are the best love lve love',
+          likes: 2,
+        },
+        {
+          id: 'asdf',
+          comment: 'you are the best love lve love',
+          likes: 2,
+        }
+      ]
+    }
+  ]
+
   return (
     <Grid
       container
       justifyContent="center"
       alignItems="center"
       spacing={4}
-      sx={{padding: {
-              sm: "0px",
-              md: "10px",
-              lg: "20px",
-            }
+      sx={{
+        padding:
+        {
+          sm: "0px",
+          md: "10px",
+          lg: "20px",
+        }
       }}
     >
-      <CrushCard />
-      <CrushCard />
-      <CrushCard />
-      <CrushCard />
-      <CrushCard />
+      {
+        testData.map(data => {
+          return (
+            <CrushCard
+              email={data.email}
+              crushCount={data.crushCount}
+              instagramLink={data.instagramLink}
+              confessions={data.confessions}
+            />
+          )
+        })
+      }
     </Grid>
   )
 }
